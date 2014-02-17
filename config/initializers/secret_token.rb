827df824a9da1e1dc89873429b1d63d423e5e3a2
Carl Allen <carl.allen@sportngin.com>
2014-02-17 11:05:08 -0600

    Allow override of secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tappapp::Application.config.secret_key_base = '85a73ad570517a35d3b8091adad3432267d86d324f1a21a6e9388fc8f9b3383998faef4d5518bdb7a0b77017f47410cbde042ad2fd0be8901a7572ef31794457'
+Tappapp::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '85a73ad570517a35d3b8091adad3432267d86d324f1a21a6e9388fc8f9b3383998faef4d5518bdb7a0b77017f47410cbde042ad2fd0be8901a7572ef31794457'
