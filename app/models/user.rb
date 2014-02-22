8c0669a2811d9917842e6698d9a3024aa2f89620
Jon Phenow <j.phenow@gmail.com>
2014-02-22 16:15:17 -0600

    validate presence and uniqueness of user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
